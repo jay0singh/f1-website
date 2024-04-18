db008d01b3fbff53ce994d21f13950316b5b3360
Jay Prakash <jaysingha261@gmail.com>
2024-04-18 23:55:26 +0530

    First Commit (Homepage done)

---
+++ script.js
@@ -0,0 +1,32 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
+
+gsap.to("#desktop-nav", {
+    backgroundColor : "#000",
+    height:"110px",
+    duration:0.5,
+    scrollTrigger: {
+        trigger:"#nav",
+        scroller:"body",
+        //markers:true,
+        start: "top -10%",
+        end : "top -11%",
+        scrub : 1
+    },
+});
+
+gsap.to("#main",{
+    backgroundColor: "#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller: "body",
+        //markers:true,
+        start: "top -25%",
+        end: "top -70%",
+        scrub:2
+    },
+});
